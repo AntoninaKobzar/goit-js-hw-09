ac92bd4d68a88191a1bd3140de219c8107f9184f
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-02-22 15:40:00 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+
+const startBtn=document.querySelector("[data-start]");
+const stopBtn=document.querySelector("[data-stop]");
+const bodyColor=document.querySelector("body");
+
+let timerId = null;
+
+startBtn.addEventListener("click",startColorChange);
+stopBtn.addEventListener("click", stopColorChange);
+
+function startColorChange() {
+timerId = setInterval(getBodyColor,1000);
+startBtn.toggleAttribute("disabled");
+};
+function stopColorChange() {
+clearInterval(timerId);
+startBtn.removeAttribute("disabled");
+};
+function getBodyColor() {
+bodyColor.style.backgroundColor = getRandomHexColor();
+};
 
