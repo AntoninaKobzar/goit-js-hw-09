c5d5b27400061360186325e993883a83f82c9a7c
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-02-26 12:19:58 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+console.log(form);
+
+form.addEventListener("submit", onFormSubmit);
+function onFormSubmit(e) {
+  e.preventDefault();
+  let inputDelay = e.currentTarget.delay.value;
+  const inputStep = e.currentTarget.step.value;
+  const inputAmount = e.currentTarget.amount.value;
+
+  for (let position = 1; position <= inputAmount; position += 1) {
+    createPromise(position, inputDelay);
+    inputDelay += inputStep;
+  };
 }
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  promise.then(({ position, delay }) => {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  }); 
+}
+
+
+    
